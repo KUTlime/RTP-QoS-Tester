05531ad088fcfcd168ce005b8afadcd12197c6a9
Radek Zahradník <kutlime@gmail.com>
2019-07-10 02:08:08 +0200

    Update of assembly info.

--- RTP QoS tester/Properties/AssemblyInfo.cs
+++ RTP QoS tester/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RTP QoS tester")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A console application for Quality of Service measurement. It provides a statistics how much packet were lost during transmission over network.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Medirecord")]
 [assembly: AssemblyProduct("RTP QoS tester")]
